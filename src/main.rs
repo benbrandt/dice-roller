bd71ce38d7a1737a1c3e8c0ffd5272c7acaf023a
Ben Brandt <benjamin.j.brandt@gmail.com>
2019-01-25 21:22:24 -0800

    Use latest aws

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,47 @@
-extern crate lambda_runtime as lambda;
-extern crate serde_derive;
-
-use lambda::{error::HandlerError, lambda, Context};
-use serde_derive::{Deserialize, Serialize};
 use std::error::Error;
 
-#[derive(Serialize, Deserialize)]
-struct RollEvent {
-    dice: String,
-}
-
-#[derive(Serialize, Deserialize)]
-struct RollResponse {
-    roll: String,
-}
+use lambda_http::{lambda, IntoResponse, Request, RequestExt, Response};
+use lambda_runtime::{error::HandlerError, Context};
+use log::{self, error};
+use serde_json::json;
+use simple_logger;
 
 fn main() -> Result<(), Box<dyn Error>> {
+    simple_logger::init_with_level(log::Level::Debug).unwrap();
     lambda!(handler);
     Ok(())
 }
 
-fn handler(event: RollEvent, ctx: Context) -> Result<RollResponse, HandlerError> {
-    if event.dice == "" {
-        return Err(ctx.new_error("No dice"));
-    }
-
-    match dice_roller::roll(&event.dice) {
-        Ok(r) => Ok(RollResponse {
-            roll: r.to_string(),
-        }),
-        Err(m) => Err(ctx.new_error(m)),
+fn handler(event: Request, ctx: Context) -> Result<impl IntoResponse, HandlerError> {
+    match event.query_string_parameters().get("dice") {
+        Some(dice) => {
+            let roll = dice_roller::roll(dice)?;
+            Ok(json!({ "roll": roll.to_string() }).into_response())
+        }
+        _ => {
+            error!("Empty dice in request {}", ctx.aws_request_id);
+            Ok(Response::builder()
+                .status(400)
+                .body("Empty dice".into())
+                .expect("failed to render response"))
+        }
     }
 }
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn handler_handles() {
+//         let request = Request::default();
+//         let expected = json!({
+//         "message": "Go Serverless v1.0! Your function executed successfully!"
+//         })
+//         .into_response();
+//         let response = handler(request, Context::default())
+//             .expect("expected Ok(_) value")
+//             .into_response();
+//         assert_eq!(response.body(), expected.body())
+//     }
+// }
